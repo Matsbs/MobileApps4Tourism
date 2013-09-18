455a241aa73e9008715e4b9b422295f0ec58efe5
Mats Sandvoll <matssandvoll@gmail.com>
2013-09-18 10:39:47 +0100

    Initial Commit

---
+++ MobileApps4Tourism/MobileApps4TourismAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MobileApps4TourismAppDelegate.h
+//  MobileApps4Tourism
+//
+//  Created by Mats Sandvoll on 18.09.13.
+//  Copyright (c) 2013 Mats Sandvoll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MobileApps4TourismAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MobileApps4TourismTests/MobileApps4TourismTests.h
@@ -0,0 +1,13 @@
+//
+//  MobileApps4TourismTests.h
+//  MobileApps4TourismTests
+//
+//  Created by Mats Sandvoll on 18.09.13.
+//  Copyright (c) 2013 Mats Sandvoll. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MobileApps4TourismTests : SenTestCase
+
+@end
